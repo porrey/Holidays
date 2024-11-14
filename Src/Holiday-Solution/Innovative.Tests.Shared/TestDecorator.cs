436be1daddc5afb9feec9612e7496eeb1a6770fd
Daniel Porrey <porrey@users.noreply.github.com>
2024-11-13 20:37:00 -0600

    Updated to net9.0

---
+++ Src/Holiday-Solution/Innovative.Tests.Shared/TestDecorator.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+
+namespace Innovative.Tests.Shared
+{
+    public class Assert2
+    {
+        public static void AreEqual(object x, object y)
+        {
+            Assert.That(x, Is.EqualTo(y));
+        }
+
+        public static void IsTrue(bool value)
+        {
+            Assert.That(value, Is.EqualTo(true));
+        }
+
+        public static void AreSame(object x, object y)
+        {
+            Assert.That(x, Is.SameAs(y));
+        }
+    }
+}
